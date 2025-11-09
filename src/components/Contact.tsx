12c3881d35f16a7aa76ca0dac8df4d8513568914
Victheem <ravoltng@gmail.com>
2025-11-09 23:11:14 +0100

    Fix: typed event parameter in Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,8 @@ const Contact = () => {
     message: "",
   });
 
-  const handleChange = (e) => {
+ const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
     const { name, value } = e.target;
     setFormData((prev) => ({ ...prev, [name]: value }));
   };
