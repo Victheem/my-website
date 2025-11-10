2f4fe520a1ae28ae1efbde7b4721f4823aa03598
Victheem <ravoltng@gmail.com>
2025-11-11 00:15:37 +0100

    Fix Reveal component export and import for Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import PartnerCarousel from "@/components/PartnerCarousel";
 import AboutSection from "@/components/AboutSection";
 import { Hero } from "@/components/hero";
-//import ServicesSection from "@/components/ServicesSection";
 import Services from "@/components/sections/Services";
 import { Projects } from "@/components/Projects";
 import { WhyChooseUs } from "@/components/WhyChooseUs";
@@ -25,7 +24,7 @@ export default function HomePage() {
 
       {/* Services */}
       <Services />
-      {/* <ServicesSection /> */}
+      
 
       {/* Why Choose Us */}
       <WhyChooseUs />
